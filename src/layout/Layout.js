52a5942dd0d9a7261d1e38d11332d61bff939a43
nguyentan2212 <nguyentan2212@gmail.com>
2022-07-13 21:20:28 +0700

    [feat] add footer and sidebar to layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
 
 function Layout() {
     return (
-    <div className="max-w-screen min-h-screen">
-        <Header/>
-        
-    </div>);
+        <div className="flex flex-col max-w-screen min-h-screen">
+            <Header />
+            <Sidebar />
+            <Footer />
+        </div>
+    );
 }
 
 export default Layout;
