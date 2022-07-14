a7b83342c8b05d3e7abc6c8e81cba1ea6e1d8712
nguyentan2212 <nguyentan2212@gmail.com>
2022-07-14 09:58:08 +0700

    [feat] add suffix

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -4,15 +4,22 @@ function Input({ prefix, suffix, placeholder, className }) {
     return (
         <div className={`relative rounded-3xl ${className}`}>
             {prefix && (
-                <span className="absolute inset-y-0 left-4 flex flex-col justify-center">
+                <span className="absolute inset-y-0 left-4 flex flex-col justify-center text-grey-dark">
                     {prefix}
                 </span>
             )}
             <input
-                className="h-full w-full pl-12 rounded-3xl bg-transparent border-transparent focus:outline-primary"
+                className={`h-full w-full rounded-3xl px-5 bg-transparent border-transparent focus:outline-primary ${
+                    prefix && "pl-12"
+                } ${suffix && "pr-12"}`}
                 type="text"
                 placeholder={placeholder}
             ></input>
+            {suffix && (
+                <span className="absolute inset-y-0 right-4 flex flex-col justify-center text-grey-dark">
+                    {suffix}
+                </span>
+            )}
         </div>
     );
 }
